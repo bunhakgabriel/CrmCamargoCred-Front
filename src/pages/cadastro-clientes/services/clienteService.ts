01b332447a0584050ad2fed4fb60b6f65b051d05
Gabriel Bunhak <bunhakgabriel@gmail.com>
2026-03-24 00:49:36 -0300

    Adiciona Classe ApiRequeste para padronizar as requisições

--- src/pages/cadastro-clientes/services/clienteService.ts
+++ src/pages/cadastro-clientes/services/clienteService.ts
@@ -1,5 +1,8 @@
+import ApiRequest from "../../../api/ApiRequeste.ts";
 import type { ICliente } from "../../../interfaces/ICliente";
 
-export async function criarCliente(data: ICliente){
-    console.log(data)
+export async function criarCliente(cliente: ICliente){
+    const api = new ApiRequest();
+    const data = await api.post<ICliente>('http://localhost:8000/cliente/cadastrar', cliente);
+    return data;
 }
