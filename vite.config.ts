e31298d36d2cda1509fcf938c9ac4f75cdfbc19c
Gabriel Bunhak <bunhakgabriel@gmail.com>
2026-06-18 19:33:14 -0300

    atualiza url vite-config e adiciona arquivo netlify

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     allowedHosts: ['d6a8-2001-1284-f514-c8e3-79e1-4944-7eeb-a716.ngrok-free.app'],
     proxy: {
       '/ConfigGestorCrmApi': {
-        target: 'http://localhost:8000',  // URL do seu backend
+        target: 'http://3.14.7.116:8000',  // URL do seu backend
         changeOrigin: true,               // Faz a troca de origem para evitar problemas com CORS
         secure: false, 
       },
