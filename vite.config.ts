7a38cb9a63fa51e071ac5c41c880e8980de24629
Gabriel Bunhak <bunhakgabriel@gmail.com>
2026-06-26 14:46:28 -0300

    atualiza vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    allowedHosts: ['d6a8-2001-1284-f514-c8e3-79e1-4944-7eeb-a716.ngrok-free.app'],
+    allowedHosts: [
+        'crmcamargocred.fun',
+        'www.crmcamargocred.fun'
+        ],
     proxy: {
       '/ConfigGestorCrmApi': {
-        target: 'http://3.14.7.116:8000',  // URL do seu backend
+        target: 'http://localhost:8000',  // URL do seu backend
         changeOrigin: true,               // Faz a troca de origem para evitar problemas com CORS
         secure: false, 
       },
